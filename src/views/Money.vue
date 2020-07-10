bac59c1f7471e859a7dff52bad75fe76ec310447
little_ice <2902978956@qq.com>
2020-07-10 20:57:13 +0800

    设置全局组件 Nav

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,13 @@
 <template>
-    <div>money</div>
+    <div>
+        money
+        <Nav></Nav>
+    </div>
 </template>
 
 <script lang="ts">
     export default {
-        name: 'Money'
+        name: 'Money',
     };
 </script>
 
