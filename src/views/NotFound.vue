a7046206427b0443fdfb88a8aa306690b2963cf0
little_ice <2902978956@qq.com>
2020-07-10 21:15:12 +0800

    添加 404 页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div>您访问的页面不存在,请检查网址是否正确</div>
+        <div><router-link to="/">返回首页</router-link></div>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'NotFound'
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
