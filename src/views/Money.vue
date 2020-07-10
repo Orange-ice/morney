e94847a5699dc88cbef724047ef806a7420541f2
little_ice <2902978956@qq.com>
2020-07-10 21:39:14 +0800

    money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        money
+    <div class="nav-wrapper">
+        <div class="content">
+            money
+        </div>
         <Nav></Nav>
     </div>
 </template>
@@ -12,5 +14,15 @@
 </script>
 
 <style lang="scss" scoped>
-
+    .nav-wrapper {
+        border: solid 1px green;
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+    }
+    .content{
+        overflow: auto;
+        flex-grow: 1;
+        border: solid 1px blue;
+    }
 </style>
