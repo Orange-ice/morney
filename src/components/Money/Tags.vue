fc6392feab982e643ac8e86496ffd6a3d7cf5187
little_ice <2902978956@qq.com>
2020-07-13 18:51:59 +0800

    完成 Tags 组件

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,21 +1,44 @@
 <template>
     <div class="tags">
         <ul class="current">
-            <li>衣</li>
-            <li>食</li>
-            <li>住</li>
-            <li>行</li>
+            <li v-for="tag in dataSource" :key="tag"
+                :class="{selected:selectedTags.indexOf(tag)>=0}"
+                @click="toggle(tag)">{{tag}}
+            </li>
         </ul>
         <div class="new">
-            <button>新增标签</button>
+            <button @click="create">新增标签</button>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-    export default {
-        name: 'Tags'
-    };
+    import Vue from 'vue';
+    import {Component, Prop} from 'vue-property-decorator';
+
+    @Component
+    export default class Tags extends Vue {
+        @Prop() dataSource: string[] | undefined;
+        selectedTags: string[] = [];
+
+        toggle(tag: string) {
+            const index = this.selectedTags.indexOf(tag);
+            if (index >= 0) {
+                this.selectedTags.splice(index, 1);
+            } else {
+                this.selectedTags.push(tag);
+            }
+        }
+
+        create() {
+            const name = window.prompt('请输入标签名');
+            if (name === '') {
+                window.alert('标签名不能为空');
+            } else if (this.dataSource) {
+                this.$emit('update:dataSource', [...this.dataSource, name]);
+            }
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
@@ -28,7 +51,8 @@
             flex-wrap: wrap;
 
             > li {
-                background-color: #d9d9d9;
+                $bg: #d9d9d9;
+                background-color: $bg;
                 $h: 24px;
                 height: $h;
                 line-height: $h;
@@ -36,6 +60,11 @@
                 padding: 0 16px;
                 margin-right: 12px;
                 margin-top: 4px;
+
+                &.selected {
+                    background: darken($bg, 50%);
+                    color: white;
+                }
             }
         }
 
