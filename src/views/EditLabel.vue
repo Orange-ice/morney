3abf0addb9623681f09110b77544fddf0653f320
little_ice <2902978956@qq.com>
2020-07-14 20:15:06 +0800

    将 tag string 改为 {id,name}

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,13 @@
+<template>
+    <Layout>删除标签</Layout>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'EditLabel'
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
