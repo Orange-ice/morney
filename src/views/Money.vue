de50bddfc12e01698e2ec71208c06c8e021720f9
little_ice <2902978956@qq.com>
2020-07-15 20:34:53 +0800

    封装 record 得操作

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -16,10 +16,8 @@
     import Types from '@/components/Money/Types.vue';
     import FormItem from '@/components/Money/FormItem.vue';
     import Tags from '@/components/Money/Tags.vue';
-    import {Component, Watch} from 'vue-property-decorator';
-    import recordListModel from '@/models/recordListModel';
+    import {Component} from 'vue-property-decorator';
 
-    const recordList = recordListModel.fetch();
     @Component({
         components: {Tags, FormItem, Types, NumberPad}
     })
@@ -27,7 +25,7 @@
 
     export default class Money extends Vue {
         tags = window.tagList;
-        recordList: RecordItem[] = recordList;
+        recordList = window.recordList;
         record: RecordItem = {
             tags: [], notes: '', type: '-', amount: 0
         };
@@ -45,12 +43,7 @@
         }
 
         saveRecord() {
-            recordListModel.create(this.record)
-        }
-
-        @Watch('recordList')
-        onRecordChange() {
-            recordListModel.save();
+            window.createRecord(this.record)
         }
     }
 
