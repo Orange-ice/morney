3af47d809c00b015c68726e2da1bcb0c52e6972a
Burt <2902978956@qq.com>
2020-08-21 17:03:31 +0800

    修改了仓库名，更改了一点配置，添加码云部署脚本

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-      ? '/MM-website/'
+      ? '/money-website/'
       : '/',
   lintOnSave: false,
   chainWebpack: config =>{
