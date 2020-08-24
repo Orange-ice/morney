37ac7e0736ae9c3f76582a01d3d3ce351b0dccd7
Burt <2902978956@qq.com>
2020-08-24 11:04:05 +0800

    添加电脑端打开提示二维码

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
     <div>
+      <div class="QRcode">
+        <p>微信扫描二维码，手机打开预览效果最佳噢</p>
+        <img src="../src/assets/QRcode.png" alt="手机打开的二维码">
+      </div>
         <router-view></router-view>
     </div>
 </template>
@@ -14,5 +18,28 @@
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         background: #FCF5F7;
+      // 让电脑端不那么太难看
+      max-width: 640px;
+      margin: 0 auto;
+    }
+    .QRcode{
+      display: none;
+    }
+    @media (min-width: 768px) {
+      .QRcode{
+        display: block;
+        position: fixed;
+        width: 120px;
+        height: 120px;
+        right: 100px;
+        top: 50px;
+        > img{
+          width: 100%;
+        }
+        > p{
+          color: green;
+          font-weight: bolder;
+        }
+      }
     }
 </style>
